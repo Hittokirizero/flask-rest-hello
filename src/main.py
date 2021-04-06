bd6b0a729274e3d69da8706432e3ae7adc5352d2
Hittokirizero <v_hbk@hotmail.com>
2021-04-06 02:50:50 +0000

    API

--- src/main.py
+++ src/main.py
@@ -39,6 +39,90 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+"""LISTO PEOPLE"""
+@app.route('/people', methods=['GET', 'POST'])
+def people_sw():
+    if request.method == 'GET':
+        gt_get_people = People_SW.query.all()
+        all_people = list(map(lambda x: x.serialize_people(), gt_get_people))
+        return jsonify(all_people), 200
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        insert_people = People_SW(id_people=request_body["id_people"],
+                                  people_height=request_body["people_height"],
+                                  people_mass=request_body["people_mass"],
+                                  people_hair_color=request_body["people_hair_color"],
+                                  people_skin_color=request_body["people_skin_color"],
+                                  people_eye_color=request_body["people_eye_color"],
+                                  people_birth_year=request_body["people_birth_year"],
+                                  people_gender=request_body["people_gender"],
+                                  people_created=request_body["people_created"],
+                                  people_edited=request_body["people_edited"],
+                                  people_name=request_body["people_name"],
+                                  people_homeworld=request_body["people_homeworld"],
+                                  people_url=request_body["people_url"])
+        db.session.add(insert_people)
+        db.session.commit()
+        return jsonify({"Todo ok" : request_body }), 200
+
+@app.route('/people/<int:id_pp>', methods=['GET'])
+def people_sw_u(id_us):
+    gt_get_people_u =  People_SW.query.filter_by(id_people = id_us).first()
+    if gt_get_people_u is None:
+        raise APIException('Lo que buscas no existe', status_code=404)
+    people = gt_get_people_u.serialize_people()
+    return jsonify(people), 200
+
+@app.route('/get_planets', methods=['GET'])
+def get_planets_sw():
+    gt_planets = Planets_SW.query.all()
+    all_planets = list(map(lambda x: x.serialize_planets(), gt_planets))
+    return jsonify(all_planets), 200
+
+@app.route('/get_planets/<int:id_pp>', methods=['GET'])
+def get_planets_sw_u(id_pp):
+    gt_get_planets_u =  Planets_SW.query.filter_by(id_planets = id_pp).first()
+    if gt_get_planets_u is None:
+        raise APIException('Lo que buscas no existe', status_code=404)
+    planets = gt_get_planets_u.serialize_planets()
+    return jsonify(planets), 200
+
+@app.route('/user', methods=['GET'])
+def user_sw():
+    gt_get_user = User_SW.query.all()
+    all_user = list(map(lambda x: x.serialize_user(), gt_get_user))
+    return jsonify(all_user), 200
+
+
+
+@app.route('/get_favorites', methods=['GET'])
+def get_favorites_sw():
+    gt_favorites = Favorite_SW.query.all()
+    all_favorites = list(map(lambda x: x.serialize_favorite(), gt_favorites))
+    return jsonify(all_favorites), 200
+"""
+@app.route('/post_task', methods=['POST'])
+def post_task_td():
+    request_body = request.get_json()
+    insert_task = Todolist(td_task=request_body["label"], is_done=request_body["done"])
+    db.session.add(insert_task)
+    db.session.commit()
+    return jsonify({"Todo ok" : request_body }), 200
+
+
+@app.route('/del_task/<int:numb>', methods=['DELETE'])
+def del_task_td(numb):
+    task = Todolist.query.filter_by(id = numb).first()
+    if task is None:
+        raise APIException('Lo que buscas no existe', status_code=404)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({"Elementos elimando numero de ID:": numb}), 200
+
+"""
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User_SW(db.Model):
-
+    __tablename__ = 'user_sw'
     id_user = db.Column(db.Integer, primary_key=True)
     name_user = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(250), nullable=False)
@@ -25,7 +25,7 @@ class User_SW(db.Model):
         }
 
 class Planets_SW(db.Model):
-
+    __tablename__ = 'planets_sw'
     id_planets = db.Column(db.Integer, primary_key=True)
     planets_name = db.Column(db.String(250), nullable=False)
     planets_diameter = db.Column(db.Integer, nullable=False)
@@ -63,7 +63,7 @@ class Planets_SW(db.Model):
 
 
 class People_SW(db.Model):
-
+    __tablename__ = 'people_sw'
     id_people = db.Column(db.Integer, primary_key=True)
     people_height = db.Column(db.Integer, nullable=False)
     people_mass = db.Column(db.Integer, nullable=False)
@@ -77,57 +77,29 @@ class People_SW(db.Model):
     people_name = db.Column(db.String(250), nullable=False)
     people_homeworld = db.Column(db.String(250), nullable=False)
     people_url = db.Column(db.String(250), nullable=False)
-    planets_id_fk = db.Column(db.Integer, nullable=False)
-    """planets_id_fk = db.Column(db.Integer, db.ForeignKey('planets_SW.id_planets'), nullable=False)
-    planets = db.relationship('planets_SW')"""
+    planets_id_fk = db.Column(db.Integer, db.ForeignKey('planets_sw.id_planets'), nullable=True)
+    planets = db.relationship('Planets_SW')
  
-def __repr__(self):
-    return '<People_SW %r>' % self.people_name
+    def __repr__(self):
+        return '<People_SW %r>' % self.people_name
 
-def serialize_people(self):
-    return {
-        "id_people": self.id_people,
-        "people_height": self.people_height,
-        "people_mass": self.people_mass,
-        "people_hair_color": self.people_hair_color,
-        "people_skin_color": self.people_skin_color,
-        "people_eye_color": self.people_eye_color,
-        "people_birth_year": self.people_birth_year,
-        "people_gender": self.people_gender,
-        "people_created": self.people_created,
-        "people_edited": self.people_edited,
-        "people_name": self.people_name,
-        "people_homeworld": self.people_homeworld,
-        "people_url": self.people_url,
-        "planets_id_fk": self.planets_id_fk
-    }
-
-class Favorite_SW(db.Model):
- 
-    id_favorite = db.Column(db.Integer, primary_key=True)
-    """user_id_fk = db.Column(db.Integer, db.ForeignKey('user_SW.id_user'), nullable=False)
-    user = db.relationship('user_SW')
-    planets_id_fk = db.Column(db.Integer, db.ForeignKey('planets_SW.id_planets'), nullable=False)
-    planets = db.relationship('planets_SW')
-    people_id_fk = db.Column(db.Integer,db.ForeignKey('people_SW.id_people'), nullable=False)
-    people = db.relationship('people_SW')"""
-    user_id_fk = db.Column(db.Integer, nullable=False)
-    planets_id_fk = db.Column(db.Integer, nullable=False)
-    people_id_fk = db.Column(db.Integer, nullable=False)
-    type_favorite = db.Column(db.String(250), nullable=False)
-
-def __repr__(self):
-    return '<Favorite_SW %r>' % self.user_id_fk
-
-def serialize_Favorite(self):
-    return {
-        "id_favorite": self.id_favorite ,
-        "user_id_fk": self.user_id_fk ,
-        "planets_id_fk": self.planets_id_fk ,
-        "people_id_fk": self.people_id_fk ,
-        "type_favorite": self.type_favorite ,
-}
-
+    def serialize_people(self):
+        return {
+            "id_people": self.id_people,
+            "people_height": self.people_height,
+            "people_mass": self.people_mass,
+            "people_hair_color": self.people_hair_color,
+            "people_skin_color": self.people_skin_color,
+            "people_eye_color": self.people_eye_color,
+            "people_birth_year": self.people_birth_year,
+            "people_gender": self.people_gender,
+            "people_created": self.people_created,
+            "people_edited": self.people_edited,
+            "people_name": self.people_name,
+            "people_homeworld": self.people_homeworld,
+            "people_url": self.people_url,
+            "planets_id_fk": self.planets_id_fk
+        }
 
 # Ultima linea #
 class User(db.Model):
@@ -145,3 +117,26 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorite_SW(db.Model):
+    __tablename__ = 'favorite_sw'
+    id_favorite = db.Column(db.Integer, primary_key=True)
+    user_id_fk = db.Column(db.Integer, db.ForeignKey('user_sw.id_user'), nullable=False)
+    planets_id_fk = db.Column(db.Integer, db.ForeignKey('planets_sw.id_planets'), nullable=False)
+    people_id_fk = db.Column(db.Integer,db.ForeignKey('people_sw.id_people'), nullable=False)
+    type_favorite = db.Column(db.String(250), nullable=False)
+    user = db.relationship('User_SW')
+    planets = db.relationship('Planets_SW')
+    people = db.relationship('People_SW')
+
+    def __repr__(self):
+        return '<Favorite_SW %r>' % self.user_id_fk
+
+    def serialize_favorite(self):
+        return {
+            "id_favorite": self.id_favorite ,
+            "user_id_fk": self.user_id_fk ,
+            "planets_id_fk": self.planets_id_fk ,
+            "people_id_fk": self.people_id_fk ,
+            "type_favorite": self.type_favorite ,
+    }
